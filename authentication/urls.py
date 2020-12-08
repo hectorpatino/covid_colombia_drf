f3818313286a5f55c6f72c83e7f24e7ee60162ae
Hector Patiño <hectorpatino24@gmail.com>
2020-12-07 19:06:49 -0500

    drop view of authentication just for now as the were only to see heroku

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,2 @@
 from django.urls import path
 from .views import *
-urlpatterns = [
-    path('', muestra, name='main'),
-]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,3 @@
 from django.http import HttpResponse
 import datetime
 
-
-def muestra(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
