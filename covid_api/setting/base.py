6a07ff7c56cd1a3112d70b796f6d1494d83c3124
Hector Patiño <hectorpatino24@gmail.com>
2020-12-08 13:03:22 -0500

    json database generated

--- covid_api/setting/base.py
+++ covid_api/setting/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'covid_co_api.apps.CovidCoApiConfig',
     'django_extensions',
--- covid_co_api/serializers/possitive_serializer.py
+++ covid_co_api/serializers/possitive_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.utils.field_mapping import get_url_kwargs
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from covid_co_api.models import Possitive, City, Gender, \
     CaseLocation, SeverityStatus, Department
 from rest_framework.reverse import reverse
@@ -27,7 +28,7 @@ class CitySerializerForPossitive(serializers.HyperlinkedModelSerializer):
 
 
 class PossitiveCaseSerializer(serializers.HyperlinkedModelSerializer):
-    nurse = serializers.SlugRelatedField(queryset=User.objects.all(),
+    nurse = serializers.SlugRelatedField(queryset=get_user_model().objects.all(),
                                          slug_field='username')
     city = CitySerializerForPossitive()
     gender = serializers.SlugRelatedField(queryset=Gender.objects.all(),
